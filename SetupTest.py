7f099905571032aed2bec6683e4201dfef37f1d2
Katifram <156585692+Katifram@users.noreply.github.com>
2024-11-11 09:58:02 +0100

    Create SetupTest.py

---
+++ SetupTest.py
@@ -0,0 +1,14 @@
+import pyscf
+from gpu4pyscf.dft import rks
+
+atom ='''
+O       0.0000000000    -0.0000000000     0.1174000000
+H      -0.7570000000    -0.0000000000    -0.4696000000
+H       0.7570000000     0.0000000000    -0.4696000000
+'''
+
+mol = pyscf.M(atom=atom, basis='def2-tzvpp')
+mf = rks.RKS(mol, xc='LDA').density_fit()
+
+e_dft = mf.kernel()  # compute total energy
+print(f"total energy = {e_dft}")
